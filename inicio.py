330d9758149d013f757b2c4639d2917ac62b23b5
perezandry2004 <perezandry2004@gmail.com>
2024-08-17 16:06:24 +0000

    menor y mayor número

--- inicio.py
+++ inicio.py
@@ -1,13 +1,1 @@
-edad = 22 
-# crea la condicion de que si es menor de 18 no puede entrar 
-if edad < 18: 
-    print("no puedes entrar") 
-else: 
-    print("puedes entrar")
-
-    nombre = input("¿Cómo te llamas? ")
-edad = int(input("¿Cuántos años tienes? "))
-
-print(f"Te llamas {nombre} y tienes {edad} años.")
- 
 
