0387917d1600d58dfa69d80df5c0eac2f16ba72f
Andry20400 <perezandry2004@gmail.com>
2024-08-31 16:49:09 +0000

    a celsius

---
+++ clase#3/clase#4/ejercicio#2.py
@@ -0,0 +1,8 @@
+def fahrenheint_a_celsius (fahrenheint): 
+     celsius = (fahrenheint - 32) * 5/9 
+     return celsius
+print ("77 grados fahrenheint es equivalente a", fahrenheint_a_celsius(77), "grados celsius") 
+print ("32 grados fahrenheint es equivalente a", fahrenheint_a_celsius(32), "grados celsius")
+
+
+
